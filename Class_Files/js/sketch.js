5e5ec819aa975fe642427fda99724ea01f0dafe5
mtc9417 <centm535@newschool.edu>
2020-06-22 15:46:03 -0400

    updated code

--- Class_Files/js/sketch.js
+++ Class_Files/js/sketch.js
@@ -1,141 +1,108 @@
-const textEle = document.getElementById('text');
-const options = document.getElementsByClassName('btn');
+//Text adventure
+//Scene Text + Two Choices
+//Manipulate Scene Text and Option text
+//Different text for different scenes
 
-for (var i = 0; i < options.length; i++) {
-    (function(index) {
-         options[index].addEventListener("mousedown", function() {
+//store the values of the elements on the webpage
+var sceneTextEle = document.getElementById("sceneText");
+var optionsTextEle = document.getElementsByClassName("btn");
+
+
+//assign a listener to each button and call the progress game function whenever they are pressed
+for(var i = 0; i < optionsTextEle.length; i++){
+    (function(index){
+        optionsTextEle[index].addEventListener("mousedown", function(){
             progressGame(index);
-          })
+        });
     })(i);
 }
 
-let sceneNum = 0;
-
-let sceneText = [
-                "My Text Adventure",//scene 0
-                "You Wake up In Bed to a new morning", //scene 1
-                "Made By Michael", //scene2 
-                "You Go back to sleep",//scene 3
-                "You Enjoy your Day", //scene 4
-                "You go to Bed", //scene 5
-                "You collapse, exhausted", //scene 6
-                ];
-
-let sceneOption1 = [
-                    "Play", //scene 0
-                    "Go Back to Sleep",//scene 1
-                    "Play", //scene 2
-                    "Play Again",//scene 3
-                    "It's Time for Bed",//scene 4
-                    "Play Again",//scene 5
-                    "Play Again",//scene 6
-                    ];
-
-let sceneOption2 = [
-                    "Credits",//scene 0
-                    "Get Up",//scene 1
-                    "Main Menu", //scene 2
-                    "Main Menu",//scene 3
-                    "Carry On",//scene 4
-                    "Main Menu",//scene 5
-                    "Main Menu",//scene 6
-                    ];
+var sceneNum = 0;
 
 
+//the text that will appear on the screen
+var textForScene = [
+                        "My Text Adventure",//scene 0
+                        "Made by Me",//scene 1 or credits
+                        "Welcome to your adventure",//scene 2
+                        "Oh a fighter?",//scene 3
+                        "Do you expect to take a hit?"//scene 4
+                   ];
+                    
+var textForOption0 = [
+                        "Play",//scene 0
+                        "Play", // scene 1 or credits
+                        "Grab a Sword",//scene 2 or credits
+                        "Attack the nearest thing",//scene 3
+                        "Start a fight"//scene 4
+                     ];
+
+var textoForOption1 = [
+                        "Credits", //scene 0
+                        "Main Menu",//scene 1
+                        "Grab a Sheild",//scene 2
+                        "The best defense is a strong offense",//scene 3
+                        "Better safe than sorry"//scene 4
+                     ];
+
+
+//initialize on scene 0
 function startGame(){
-    displayText(sceneNum);
-    console.log(sceneNum);
+    updateText();
 }
 
+//check which scene we are on, check which button was pressed, go to that scene number
 function progressGame(whichOption){
-
-    switch(sceneNum) {
+    switch(sceneNum){
         case 0:
-            if(whichOption==0){
-                sceneNum = 1;
-                displayText(sceneNum);
-            }
-            else if(whichOption == 1){
+            if(whichOption == 0){
                 sceneNum = 2;
-                displayText(sceneNum);
-            }
-          break;
-
-        case 1:
-            if(whichOption==0){
-                sceneNum = 3;
-                displayText(sceneNum);
-            }
-            else if(whichOption == 1){
-                sceneNum = 4;
-                displayText(sceneNum);
-            }
-          break;
-
-        case 2:
-            if(whichOption==0){
+            }else{
                 sceneNum = 1;
-                displayText(sceneNum);
-            }
-            else if(whichOption == 1){
-                sceneNum = 0;
-                displayText(sceneNum);
-            }
-          break;
-
-        case 3:
-            if(whichOption==0){
-                sceneNum = 1;
-                displayText(sceneNum);
-            }
-            else if(whichOption == 1){
-                sceneNum = 0;
-                displayText(sceneNum);
-            }
-          break;
-
-        case 4:
-            if(whichOption==0){
-                sceneNum = 5;
-                displayText(sceneNum);
-            }
-            else if(whichOption == 1){
-                sceneNum = 6;
-                displayText(sceneNum);
-            }
-          break;
-
-        case 5:
-            if(whichOption==0){
-                sceneNum = 1;
-                displayText(sceneNum);
-            }
-            else if(whichOption == 1){
-                sceneNum = 0;
-                displayText(sceneNum);
-            }
-          break;
-
-        case 6:
-            if(whichOption==0){
-                sceneNum = 1;
-                displayText(sceneNum);
-            }
-            else if(whichOption == 1){
-                sceneNum = 0;
-                displayText(sceneNum);
             }
         break;
 
-      }
+        case 1:
+            if(whichOption == 0){
+                sceneNum = 2;
+            }else{
+                sceneNum = 0;
+            }
+        break;
 
-    console.log(sceneNum);
+        case 2:
+            if(whichOption == 0){
+                sceneNum = 3;
+            }else{
+                sceneNum = 4;
+            }
+        break;
+
+        case 3:
+            if(whichOption == 0){
+                sceneNum = 0;
+            }else{
+                sceneNum = 0;
+            }
+        break;
+
+        case 4:
+            if(whichOption == 0){
+                sceneNum = 0;
+            }else{
+                sceneNum = 0;
+            }
+        break;
+    }
+    updateText();
 }
 
-function displayText(sceneValue){
-    textEle.innerHTML = sceneText[sceneValue];
-    options[0].innerHTML = sceneOption1[sceneValue];
-    options[1].innerHTML = sceneOption2[sceneValue];   
+
+//change the text on screen based on which scene we are on
+function updateText(){
+    sceneTextEle.innerHTML = textForScene[sceneNum];
+    optionsTextEle[0].innerHTML = textForOption0[sceneNum];
+    optionsTextEle[1].innerHTML = textoForOption1[sceneNum];
 }
 
 startGame();
